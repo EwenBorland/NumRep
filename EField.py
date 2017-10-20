f841d044c33d3c64ca8a42a0b55445ecf570c27f
Ewen Borland <s1339114@cplab214.ph.ed.ac.uk>
2017-10-20 11:46:18 +0100

    things

--- EField.py
+++ EField.py
@@ -4,9 +4,11 @@ from time import sleep
 import ChargeDistribution as ChDist
 
 
+
 #setup charge distribution
 rho = ChDist.ChargeDistribution()
-#rho.show(title='title',disp=True)
+#plt.figure(3)
+#rho.show(title='title',disp=False)
 
 #define ODE
 def dE(x):
@@ -34,7 +36,7 @@ init = [-2.0,0.0]
 r = np.linspace(min(xminmax),max(xminmax),ncount)
 for i in range(len(r)):
 	if r[i] >= init[0]:
-		print i
+		#print i
 		leftlist = r[0:i]
 		rightlist = r[i:]
 		break
@@ -78,17 +80,20 @@ for i in range(len(x_vals)):
 
 
 ####plotting garbage
+
+plt.figure(1)
 plt.plot(r,y_vals,'r-')
 #plt.plot(r,diffy,'y-')
 #plt.plot(r,y_vals2,'g-')
 #plt.plot([0,0],[0,0.6],'b-')
 #print max(diffx)
 #print max(diffy)
-#plt.figure(1)
 
 
 
-E_y_list = y_vals
+
+
+E_y_list = [i*-1 for i in y_vals]
 Vy_vals = [0]
 #function to return a value from a list
 #the step funcitions require a function to work so this is a workaround instead of rewriting the step funcitons or writing new, list based ones.
@@ -102,32 +107,41 @@ def step_RK4_V(x0,y0,func):
 	k2 = func(x0 + 1)
 	k3 = func(x0 + 1)
 	k4 = func(x0 + 2)
-	y1 = y0 + (k4-k1)*(k1/6 + k2/3 + k3/3 + k4/6)
+	y1 = y0 + (stepsize*2)*(k1/6 + k2/3 + k3/3 + k4/6)
 	return y1
 
 
 
-for n in range(len(r)-2):
+for n in range((len(r)-2)/2):
 	y_n = Vy_vals[n]
-	x_n = r[n]
+	x_n = r[n*2]
 	#y_vals.append(Euler_next_y(y_n,x_n,dE,stepsize))
 	Vy_vals.append(step_RK4_V(n,y_n,E_y))
 	#Vx_vals.append(x_n + stepsize)
 	print n
-	n+=1
-rnew = r[:399]
+	#n+=1
+#rnew = r[:399]
+#rnew=np.arange((len(r)-2)/2)[:199]
+print r
+rnew = np.linspace(min(xminmax),max(xminmax),ncount/2)
+print rnew
+
 print "lengths"
 print len(rnew)
 print len(Vy_vals)
+plt.figure(2)
 plt.plot(rnew,Vy_vals,'g-')
-#plt.figure(2)
 
 
 
 
-plt.ylim(-0.1,0.8)
 
 
+#plt.ylim(-0.1,0.8)
+plt.figure(4)
+rho.show(title='title',disp=False)
+plt.plot(r,y_vals,'r-')
+plt.plot(rnew,Vy_vals,'g-')
 
-
-plt.show()
+plt.show(block=False)
+input()
