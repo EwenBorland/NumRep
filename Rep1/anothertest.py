42ae8fb31c02c4027f23ef9bd889ae03ce765795
Ewen Borland <s1339114@cplab001.ph.ed.ac.uk>
2017-11-14 14:09:46 +0000

    kjhgfd

--- Rep1/anothertest.py
+++ Rep1/anothertest.py
@@ -2,100 +2,78 @@ from scipy import misc
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft ,ifft
 import numpy as np
-
+# 184,216
 #filename
 fn = "desync1.pgm"
 #Reading file and making it a numpy array
 im = misc.imread(fn)
 im_org = misc.imread(fn)
-
+filey = open("rep1testout.txt",'w')
 def shifter(l,i):
 	#print "a"
-	c = 0
+	p = 0
 	if i == 0:
 		#print "nope"
 		return l
 	elif i > 0:
 		#print "right"
-		while c < i:
+		while p <= i:
 			#print"aa"
 			l = np.delete(l,len(l)-1)
 			#print "ab"
-			l = np.insert(l,0,255)
+			l = np.insert(l,0,100)
 			#print "ac"
-			c+=1
+			p+=1
 	elif i < 0:
 		#print "left"	
-		while c < abs(i):
+		while p <= abs(i):
 			#print "ba" 
 			l = np.delete(l,0)
 			#print "bb"
-			l = np.append(l,0)
+			l = np.append(l,100)
 			#print "bc"
-			c+=1
+			p+=1
 	return l
 
 transformed = []
 
 for row in im:
-	transformed.append(fft(row))
+	transformed.append(fft(np.copy(row)))
 
 shift_vals = []
-
+i_ = []
+i=0
+rowlen = len(im[0])
 for c, row in enumerate(transformed):
 	if c == 0:
-		row_p = row
-		shift_vals.append(0)
+		row_p = np.copy(row)
+		shift_num = 0
+		
 	else:
-		con = np.conjugate(row_p)
+		con = np.conjugate(np.copy(row_p))
 		eq = con*row
 		shift_num = np.argmax(ifft(eq))
-		shift_vals.append(shift_num)
-
-def shifter(l,i):
-	#print "a"
-	c = 0
-	if i == 0:
-		#print "nope"
-		return l
-	elif i > 0:
-		#print "right"
-		while c < i:
-			#print"aa"
-			l = np.delete(l,len(l)-1)
-			#print "ab"
-			l = np.insert(l,0,255)
-			#print "ac"
-			c+=1
-	elif i < 0:
-		#print "left"	
-		while c < abs(i):
-			#print "ba" 
-			l = np.delete(l,0)
-			#print "bb"
-			l = np.append(l,255)
-			#print "bc"
-			c+=1
-	else:
-		print "bork"
-	return l
-
-
-
-
-
-#print shift_vals
-rowlen = len(im[0])
-i=0
-for c, shft in enumerate(shift_vals):
-	if shft > rowlen-(rowlen*0.2):
-		i += rowlen - shft
-	elif shft <= rowlen-(rowlen*0.8):
-		i -= -1*shft
+	shift_vals.append(shift_num)
+	
+	if shift_num > rowlen-(rowlen*0.1):
+		i += (rowlen - shift_num)
+		#i += (-1*shift_num)
+		#i += shift_num - rowlen
+	elif shift_num <= rowlen-(rowlen*0.9):
+		i += (-1*shift_num)
+		#i += (rowlen - shift_num)
+		#i += shift_num
 	else:
 		i += 0
-	im[c] = shifter(im[c],i)
-	
+	i_.append(i)
+		
+	filey.write("row:{0} \t maxarg:{1} \t i:{2}\n".format((c+1),shift_num,i))
+#print shift_vals
+filey.close()
+print len(i_)
+for c, i in enumerate(shift_vals):
+	im[c] = np.copy(shifter(im[c],i))
+
 
 
 '''
@@ -127,5 +105,9 @@ plt.imshow(im_org,cmap=plt.cm.gray)
 plt.figure()
 plt.imshow(la,cmap=plt.cm.gray)
 '''
+
 plt.imshow(im,cmap=plt.cm.gray)
-plt.show()
+plt.figure()
+plt.imshow(im_org,cmap=plt.cm.gray)
+plt.show(block=False)
+input()
--- Rep1/rep1test.py
+++ Rep1/rep1test.py
@@ -6,7 +6,7 @@ import numpy as np
 
 #         Get the filename as string
 #fn = str(raw_input("File : "))
-fn = "desync4.pgm"
+fn = "desync1.pgm"
 #         Read file it np array
 im = scipy.misc.imread(fn) 
 print len(im[0])
@@ -35,6 +35,32 @@ print len(im[0])
 #plt.show()
 cl=[]
 ar_iftl = []
+def shifter(l,i):
+	#print "a"
+	c = 0
+	if i == 0:
+		#print "nope"
+		return l
+	elif i > 0:
+		#print "right"
+		while c <= i:
+			#print"aa"
+			l = np.delete(l,len(l)-1)
+			#print "ab"
+			l = np.insert(l,0,130)
+			#print "ac"
+			c+=1
+	elif i < 0:
+		#print "left"	
+		while c <= abs(i):
+			#print "ba" 
+			l = np.delete(l,0)
+			#print "bb"
+			l = np.append(l,130)
+			#print "bc"
+			c+=1
+	return l
+	
 def shift(xs, n):
     if n >= 0:
         return np.r_[np.full(n, 0), xs[:-n]]
@@ -50,9 +76,16 @@ for c,row in enumerate(im):
 		ar_m = pr_ft*cr_con
 		ar_m_ift = ifft(ar_m)
 		cl.append(c)
+		
 		maxift = ar_m_ift.tolist().index(max(ar_m_ift))
+		print maxift
+		print np.argmax(ar_m_ift)
 		ar_iftl.append(maxift)
 		if maxift > 0:
+			#if maxift > 256:
+			#	im[c] = shifter(row,512-maxift)
+			#else:
+			#	im[c] = shifter(row,-maxift)
 			im[c] = np.roll(row,maxift-512)
 			#if maxift > 20:
 			#	im[c] = shift(row,maxift-512)
