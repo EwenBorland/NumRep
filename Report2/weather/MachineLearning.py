1d243079dd16f48f3ace5e68818d5cd5ed8e836c
Ewen Borland <s1339114@cplab209.ph.ed.ac.uk>
2017-11-28 13:42:05 +0000

    added more classifiers, commented

--- Report2/weather/MachineLearning.py
+++ Report2/weather/MachineLearning.py
@@ -1,13 +1,10 @@
 
 
-## Step 1 - Import and Data loading
-
-# Import the sklearn modules you intend to use as part of your Machine Learning analysis
-# (e.g. classifiers, metrics, model selection)
+#Importing packages
 import graphviz
 import pickle
-# ADD SKLEARN MODULES FOR YOUR CHOSEN CLASSIFICATION METHOD HERE
-# e.g. to load the decision tree estimator use: from sklearn.tree import DecisionTreeClassifier
+
+## sklearn classification packages
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
@@ -15,15 +12,18 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
-# Load the weather data you created by FeatureExtraction.py
+from sklearn.neighbors import KNeighborsClassifier , RadiusNeighborsClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neural_network import MLPClassifier
+#Loading weather data created from FeatureExtraction.py
 weather = pickle.load(open('data/mldata.p'))
-print weather.data[0]
+
 
 # Confirm that the data has loaded correctly by inspecting the data attributes in the `weather` object.
 print weather.getNrEntries()
 print weather.getTargetNames()
 print weather.getFeatures()
-#print weather.data
+
 
 ## Step 2 - Define the training and testing sample
 #
@@ -33,58 +33,35 @@ print weather.getFeatures()
 # *Examples*:
 # [`sklearn.model_selection.train_test_split`](http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html)
 
+
+
 # DEFINE TRAINING AND TESTING SAMPLES AND TARGETS HERE
-classifications = []
+
+#separating the feature data from the weather type data
+obs_types = []
 print "total length all: {0}".format(len(weather.data))
 for i in range(len(weather.data)):
-	classification  = weather.data[i][-1] 
-	classifications.append(classification)
+	given_type  = weather.data[i][-1] 
+	obs_types.append(given_type)
 weather.delete('Weather Type')
-weather_train , weather_test, result_train, result_test= train_test_split(weather.data,classifications,test_size=0.5)
-print "total length train: {0}".format(len(weather_train))
-print "total length test: {0}".format(len(weather_test))
-print "total length result train: {0}".format(len(result_train))
-print "total length result test: {0}".format(len(result_test))
 
-#print weather.data
-#print classifications
-## Step 3 - Define the classification method
+#creating the test and train files
+weather_train , weather_test, result_train, result_test= train_test_split(weather.data,obs_types,test_size=0.3)
 
-# This can be any of the estimators provided by Sklearn.
-# I suggest you start with a *white box* method
-# to better understand the process before trying something more advanced.
 
-# DEFINE CLASSIFIER HERE
-# e.g for a Decision tree: clf = DecisionTreeClassifier()
-clf = DecisionTreeClassifier()
-clf1 = RandomForestClassifier()
-pred2 = OneVsRestClassifier(LinearSVC(random_state=0)).fit(weather_train,result_train).predict(weather_test)
+# list of the classification methods to use
+allclassifiers = [DecisionTreeClassifier,RandomForestClassifier,OneVsRestClassifier,RadiusNeighborsClassifier(radius=10.0)]
+classifiers = [DecisionTreeClassifier(),RandomForestClassifier(),KNeighborsClassifier(n_neighbors=10),BaggingClassifier(KNeighborsClassifier(),max_samples=0.5, max_features=0.5),MLPClassifier()]
+names = ["Decision Tree Classifier","Random Forest Classifier","K Nearest Neighbor Classifier","Bagging Classifier","MLP Classifier (Neural Network)"]
+predictions = []
 
-## Step 4 - Fit the training data
+# creating predicted data sets for each classifier
+# initialises a classifier, fits the classifier to the training set, then predicts a data set using the test data.
+ 
+for classifier in classifiers:
+	predictions.append(classifier.fit(weather_train,result_train).predict(weather_test))
 
-# Run the `fit` method of your chosen estimator using the training data (and corresponding targets) as input
-clf.fit(weather_train,result_train)
-clf1.fit(weather_train,result_train)
 
-# RUN FIT METHOD HERE
-
-## Step 5 - Define the expected and predicted datasets
-
-# Define `expected` as your *test* target values (i.e. **not** your *training* target values)
-# and run the `predict` method on your chosen estimator using your *test data*
-
-# DEFINE EXPECTED AND PREDICTED VALUES HERE
-pred = clf.predict(weather_test)
-pred1 = clf1.predict(weather_test)
-#dot_data = export_graphviz(clf, out_file="clf1.dot")
-#pred2 = clf2.predict(weather_test)
-print pred2
-print "Decision Tree"
-print classification_report(result_test,pred)
-print "\nRandom Forest Class"
-print classification_report(result_test,pred1)
-print "\nOneVsRest Classifier"
-print classification_report(result_test,pred2)
 
 ## Step 6 - Prediction Evaluation
 
@@ -93,5 +70,21 @@ print classification_report(result_test,pred2)
 # Examples:
 # - [Sklearn Model Evaluation](http://scikit-learn.org/stable/modules/model_evaluation.html#)
 # - [Handwritten Digits example](http://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html#sphx-glr-auto-examples-classification-plot-digits-classification-py)
-
+#dot_data = export_graphviz(clf, out_file="clf1.dot")
 # RUN PREDICTION EVALUATION METHODS HERE
+
+#printing classification_report() results to the console for each classifier
+for i in range(len(classifiers)):
+	print "---------------------------------------------"
+	#print classifiers[i].__name__
+	print names[i]
+	print classification_report(result_test,predictions[i])
+
+
+
+
+
+
+
+
+
